1089063e74ca04691ade88dace3f104df241e651
kinoshita0623 <kuutarousii@gmail.com>
2020-11-02 18:58:37 +0900

    Login用のViewModelを作成した

--- app/src/main/java/jp/kinoshita/linksharingandroidclient/application/LoginViewModel.kt
+++ app/src/main/java/jp/kinoshita/linksharingandroidclient/application/LoginViewModel.kt
@@ -3,7 +3,14 @@ package jp.kinoshita.linksharingandroidclient.application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import jp.kinoshita.linksharingandroidclient.model.LinkSharingAPI
+import jp.kinoshita.linksharingandroidclient.model.login.Login
+import jp.kinoshita.linksharingandroidclient.model.login.Token
+import jp.kinoshita.linksharingandroidclient.model.users.User
 
 class LoginViewModel(
     val api: LinkSharingAPI
@@ -13,15 +20,49 @@ class LoginViewModel(
 
     val password = MutableLiveData<String>()
 
+    var deviceName = "LinkSharing4Android"
+
 
     private val mIsLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = mIsLoading
 
+    private val mCompositeDisposable = CompositeDisposable()
+
+    private val mLoggedInTokenSubject = PublishSubject.create<Token>()
+    private val loggedInToken: Observable<Token> = mLoggedInTokenSubject
+
 
 
     fun login(){
+        val email = this.email.value?: ""
+        val password = this.password.value?: ""
+
+
+        mIsLoading.value = true
+
+
+        val disposable = api.login(
+            Login(email = email, password = password, deviceName = this.deviceName)).subscribeOn(Schedulers.io())
+            .subscribe { t1, t2 ->
+                val body = t1.body()
+                if(body == null|| t2 != null){
+                    mLoggedInTokenSubject.onError(t2)
+
+                }else if(t1.code() in 200 until 300){
+                    mLoggedInTokenSubject.onNext(body)
+                }
+                mIsLoading.postValue(false)
+            }
+
+        mCompositeDisposable.add(disposable)
 
     }
+
+    fun dispose(){
+        mCompositeDisposable.clear()
+    }
+
+
 
 
 }
--- app/src/main/java/jp/kinoshita/linksharingandroidclient/model/login/Login.kt
+++ app/src/main/java/jp/kinoshita/linksharingandroidclient/model/login/Login.kt
@@ -1,6 +1,10 @@
 package jp.kinoshita.linksharingandroidclient.model.login
 
+import com.google.gson.annotations.SerializedName
+
 data class Login(
     val email: String,
-    val password: String
+    val password: String,
+    @SerializedName("device_name")
+    val deviceName: String
 )
